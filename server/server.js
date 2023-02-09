2edc541415a657d093c0c3c4d620daa8b35ee466
onkar kotule <onkar.kotule@bitcs.in>
2023-02-09 20:37:15 +0530

    mail functionality

--- server/server.js
+++ server/server.js
@@ -1,28 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
+const dotenv = require("dotenv");
 const nodemailer = require("nodemailer");
-
+dotenv.config()
 // server used to send send emails
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+
+// console.log(process.env.EMAIL_USER);
+// console.log(process.env.EMAIL_PASS);
+
+// const contactEmail = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: "onkardev95@gmail.com",
+//     pass: "Onkar@123"
+//   },
+// });
 
 const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.SMPT_HOST,
+  port: process.env.SMPT_PORT,
+  service: process.env.SMPT_SERVICE,
+  secure: true,
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: process.env.SMPT_MAIL,
+    pass: process.env.SMPT_PASSWORD,
   },
 });
-
 contactEmail.verify((error) => {
   if (error) {
-    console.log(error);
+    console.log(error.message);
   } else {
     console.log("Ready to Send");
   }
@@ -35,7 +49,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: process.env.SMPT_MAIL,
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
@@ -46,6 +60,7 @@ router.post("/contact", (req, res) => {
     if (error) {
       res.json(error);
     } else {
+      // console.log(first)
       res.json({ code: 200, status: "Message Sent" });
     }
   });
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { useEffect } from "react";
 
 export const Contact = () => {
   const formInitialDetails = {
@@ -22,24 +23,57 @@ export const Contact = () => {
       [category]: value
     })
   }
+  useEffect(() => {
+    setTimeout(() => {
+      setStatus({})
+    }, 10000)
+  }, [status])
+
+  const check = () => {
+    if (formDetails.email === "") {
+      setStatus({ succes: false, message: 'please provide your email' });
+      return false;
+    } else if (formDetails.firstName === "") {
+      setStatus({ succes: false, message: 'please provide your name' });
+      return false;
+    } else if (formDetails.lastName === "") {
+      setStatus({ succes: false, message: 'please provide your name' });
+      return false;
+    } else if (formDetails.message === "") {
+      setStatus({ succes: false, message: 'describe your purpose' });
+      return false;
+    } else if (formDetails.phone === "" && formDetails.phone.length === 10) {
+      setStatus({ succes: false, message: 'please provide your valid contact no' });
+      return false;
+    }
+    return true
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully' });
+    console.log(formDetails)
+    if (check()) {
+      let response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(formDetails),
+      });
+      setButtonText("Send");
+      let result = await response.json();
+      console.log(result)
+      setFormDetails(formInitialDetails);
+
+      if (result.code == 200) {
+        setStatus({ succes: true, message: 'Message sent successfully' });
+      } else {
+        setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
+      }
     } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
+      // setStatus({ succes: false, message: 'fill all the details' });
+      setButtonText("Send");
     }
   };
 
@@ -65,7 +99,7 @@ export const Contact = () => {
                         <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                        <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
                         <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
@@ -77,13 +111,13 @@ export const Contact = () => {
                         <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                         <button type="submit"><span>{buttonText}</span></button>
                       </Col>
-                      {
-                        status.message &&
-                        <Col>
-                          <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                        </Col>
-                      }
                     </Row>
+                    {
+                      status.message &&
+                      <Col>
+                        <h4 style={status.succes ? { color: "#06d429" } : { color: "#c30c0c" }}>{status.message}</h4>
+                      </Col>
+                    }
                   </form>
                 </div>}
             </TrackVisibility>
