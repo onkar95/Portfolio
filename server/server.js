5b7ee070ba64df23c558d5d106011c313abc5256
onkar kotule <onkar.kotule@bitcs.in>
2023-02-10 01:28:38 +0530

    link of site

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ dotenv.config()
 
 const app = express();
 const corsoption = {
-  origin: ["http://localhost:3000"],
+  origin: ["https://enchanting-ganache-626382.netlify.app/", "http://localhost:3000"],
   Credential: true
 }
 app.use(cors(corsoption));
