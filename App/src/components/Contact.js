5415ddbfe13b87ef2aeb7a7173f0199332655bd6
onkar kotule <onkar.kotule@bitcs.in>
2023-02-09 23:12:32 +0530

    deploying

--- App/src/components/Contact.js
+++ App/src/components/Contact.js
@@ -42,7 +42,7 @@ export const Contact = () => {
     } else if (formDetails.message === "") {
       setStatus({ succes: false, message: 'describe your purpose' });
       return false;
-    } else if (formDetails.phone === "" && formDetails.phone.length === 10) {
+    } else if (formDetails.phone === "" || formDetails.phone.length !== 10) {
       setStatus({ succes: false, message: 'please provide your valid contact no' });
       return false;
     }
@@ -55,6 +55,7 @@ export const Contact = () => {
     console.log(formDetails)
     if (check()) {
       let response = await fetch("https://portfolio-mail-t2t7.onrender.com/mail/contact", {
+        // let response = await fetch("http://localhost:5000/mail/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
@@ -66,7 +67,7 @@ export const Contact = () => {
       console.log(result)
       setFormDetails(formInitialDetails);
 
-      if (result.code == 200) {
+      if (result.code === 200) {
         setStatus({ succes: true, message: 'Message sent successfully' });
       } else {
         setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
@@ -105,7 +106,7 @@ export const Contact = () => {
                         <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                        <input type="number" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
                       </Col>
                       <Col size={12} className="px-1">
                         <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
