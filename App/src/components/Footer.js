3c05e90adef0627d2eaa55c9e6e89d151bf0112f
onkar kotule <66904041+onkar95@users.noreply.github.com>
2025-04-08 22:45:52 +0530

    Revert "Resume html"

---
+++ App/src/components/Footer.js
@@ -0,0 +1,31 @@
+import { Container, Row, Col } from "react-bootstrap";
+// import { MailchimpForm } from "./MailchimpForm";
+import logo from '../assets/logo/okn2.png';
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from '../assets/img/git2.svg';
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          {/* <MailchimpForm /> */}
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+            {/* <h1>Onkar Kotule</h1>
+            <h3>Developer</h3> */}
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a className="social_icon_a" href="https://www.linkedin.com/in/onkar-kotule-564608178/" target="_blank" rel="noreferrer"><img src={navIcon1} alt="" /></a>
+              <a className="social_icon_a github" href="https://github.com/onkar95" target="_blank" rel="noreferrer"><img src={navIcon2} alt="" /></a>
+              <a className="social_icon_a" href="https://www.instagram.com/onkar_95_/" target="_blank" rel="noreferrer"><img src={navIcon3} alt="" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
